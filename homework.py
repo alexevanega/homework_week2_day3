fca181b5d33f3292112f6338fc65b84133392fcb
alexevanega <alexevanega@gmail.com>
2021-11-18 18:22:50 -0500

    first commit

---
+++ homework.py
@@ -0,0 +1,13 @@
+def sq_footage(length, width):
+    area = length * width
+    return area
+
+def circumfrence(radius):
+    pi = 22/7
+
+    circumfrence = 2 * radius * pi
+
+    return circumfrence
+    
+
+
